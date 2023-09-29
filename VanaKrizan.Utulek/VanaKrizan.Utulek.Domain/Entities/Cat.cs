6721b8c62c04223f82de0a18064b6b3820310835
Rhubaruth <kubik.vana@gmail.com>
2023-09-29 16:35:50 +0200

    added Cat.sc

---
+++ VanaKrizan.Utulek/VanaKrizan.Utulek.Domain/Entities/Cat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VanaKrizan.Utulek.Domain.Entities
+{
+    public class Cat : Entity<int>, Pet
+    {
+
+    public int SizeId { get; set; }
+    public bool chip { get; set; }
+
+    // public double Cost { get; set; }
+
+    }
+}
