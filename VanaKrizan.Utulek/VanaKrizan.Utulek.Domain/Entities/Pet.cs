26d38ebe085561e3cbb40ab88724bd402d26626d
Rhubaruth <kubik.vana@gmail.com>
2023-09-29 16:32:49 +0200

    interface for Pets

---
+++ VanaKrizan.Utulek/VanaKrizan.Utulek.Domain/Entities/Pet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VanaKrizan.Utulek.Domain.Entities
+{
+    interface Pet
+    {
+        public string Name { get; set; }
+        public DateTime Birth { get; set; }
+        public int BreedId { get; set; }
+        public char Sex { get; set; }
+
+        public DateTime InShelterSince { get; set; }
+
+        // public double Cost { get; set; }
+
+        public string ImageSrc { get; set; }
+        public string Info { get; set; }
+
+        
+
+
+
+    }
+}
