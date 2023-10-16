db060d6defe10d6b0dfb41f14a1e47a5ef34d078
Katerina Krizanova <k_krizanova@utb.cz>
2023-10-16 21:28:09 +0200

    new folder and starting home page

---
+++ VanaKrizan.Utulek/VanaKrizan.Utulek.Web/Areas/Mazlicci/Controllers/MazlicciController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VanaKrizan.Utulek.Web.Areas.Mazlicci.Controllers
+{
+    public class MazlicciController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
