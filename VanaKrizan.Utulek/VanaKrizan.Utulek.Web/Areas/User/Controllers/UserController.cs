3f0de9f51ed32908f35d2130065a211e888c6938
rhubaruth <kubik.vana@gmail.com>
2024-01-06 12:02:49 +0100

    Area User

---
+++ VanaKrizan.Utulek/VanaKrizan.Utulek.Web/Areas/User/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using VanaKrizan.Utulek.Application.Abstraction;
+using VanaKrizan.Utulek.Application.Implementation;
+using VanaKrizan.Utulek.Application.ViewModels;
+using VanaKrizan.Utulek.Web.Models;
+
+namespace VanaKrizan.Utulek.Web.Controllers
+{
+    [Area("User")]
+    public class UserController : Controller
+    {
+        IHomeService _homeService;
+        private IPetService _petService;
+
+        public UserController(IHomeService homeService, IPetService petService)
+        {
+            _homeService = homeService;
+            _petService = petService;
+        }
+
+        public IActionResult MyPets()
+        {
+            CarouselProductViewModel viewModel = _homeService.GetHomeIndexViewModel(_petService);
+
+            return View(viewModel);
+        }
+
+        public IActionResult MyData()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
