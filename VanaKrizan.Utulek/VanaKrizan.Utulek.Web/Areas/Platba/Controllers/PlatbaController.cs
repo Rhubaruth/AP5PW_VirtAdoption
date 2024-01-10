eae0784456e0d160694846e8016fee2eebdb34e6
Katerina Krizanova <k_krizanova@utb.cz>
2024-01-10 14:04:38 +0100

    ups, tady oprava

--- VanaKrizan.Utulek/VanaKrizan.Utulek.Web/Areas/Platba/Controllers/PlatbaController.cs
+++ VanaKrizan.Utulek/VanaKrizan.Utulek.Web/Areas/Platba/Controllers/PlatbaController.cs
@@ -22,7 +22,6 @@ namespace VanaKrizan.Utulek.Web.Areas.Mazlicci.Controllers
 
         public IActionResult Platba()
         {
-            return View();
             PetConjoined? petConj = JsonConvert.DeserializeObject<PetConjoined>((string)TempData["PetData"]);
             if (petConj == null)
                 return NotFound();
